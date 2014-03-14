763e5fae0ec85cf2f1681be084c9b13aa3b6ff93
Richard Brown <richard.allan.brown@gmail.com>
2014-03-14 20:12:49 +0000

    Initial Commit

---
+++ Game2048Solver/GameBoard.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game2048Solver
+{
+    public struct Coord2D
+    {
+        public int x;
+        public int y;
+    }
+
+    public enum Direction
+    {
+        Left,
+        Right,
+        Up,
+        Down
+    }
+
+    public interface IGameBoard
+    {
+        bool GameOver { get; }
+        int MaxValue { get; }
+        bool Move(Direction direction);
+        int Moves { get; }
+        int Score { get; }
+        string ToString();
+    }
+
+    public class GameBoard : IGameBoard
+    {
+        private const int BOARD_LENGTH = 4;
+        private const int SPAWN_NO = 2;
+
+        private Random rand;
+        private int[,] board;
+
+        public int MaxValue { get; private set; }
+        public bool GameOver { get; private set; }
+        public int Score { get; private set; }
+        public int Moves { get; private set; }
+
+        public GameBoard()
+            : this(new Random())
+        {
+        }
+
+        public GameBoard(Random rand)
+        {
+            this.rand = rand;
+            GameOver = false;
+            MaxValue = SPAWN_NO;
+            Score = 0;
+            Moves = 0;
+
+            board = new int[BOARD_LENGTH, BOARD_LENGTH];
+
+            AddNewSquare();
+            AddNewSquare();
+        }
+
+        private void AddNewSquare()
+        {
+            SetSquare(GetRandomEmptySquare(), SPAWN_NO);
+        }
+
+        private Coord2D GetRandomEmptySquare()
+        {
+            List<Coord2D> emptySpaces = new List<Coord2D>();
+
+            for (int x = 0; x < BOARD_LENGTH; x++)
+                for (int y = 0; y < BOARD_LENGTH; y++)
+                    if (board[x, y] == 0)
+                        emptySpaces.Add(new Coord2D() { x = x, y = y });
+
+            if (emptySpaces.Count == 0)
+                throw new InvalidOperationException("Cannot get random empty square when board is full");
+
+            return emptySpaces[rand.Next(emptySpaces.Count)];
+        }
+
+        private void SetSquare(Coord2D coord, int value)
+        {
+            SetSquare(coord.x, coord.y, value);
+        }
+
+        private void SetSquare(int x, int y, int value)
+        {
+            board[x, y] = value;
+        }
+
+        private int GetSquare(Coord2D coord)
+        {
+            return GetSquare(coord.x, coord.y);
+        }
+
+        private int GetSquare(int x, int y)
+        {
+            return board[x, y];
+        }
+
+        public bool Move(Direction direction)
+        {
+            bool stateChange = false;
+
+            switch (direction)
+            {
+                case Direction.Down:
+                    stateChange = Down();
+                    break;
+
+                case Direction.Up:
+                    stateChange = Up();
+                    break;
+
+                case Direction.Left:
+                    stateChange = Left();
+                    break;
+
+                case Direction.Right:
+                    stateChange = Right();
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException("TILT! How did you get here?");
+            }
+
+            if (stateChange)
+            {
+                AddNewSquare();
+                UpdateMaxScore();
+                CheckForGameOver();
+                Moves++;
+            }
+
+            return stateChange;
+        }
+
+        private bool Left()
+        {
+            bool modification = false;
+
+            for (int y = 1; y < BOARD_LENGTH; y++)
+            {
+                for (int x = 0; x < BOARD_LENGTH; x++)
+                {
+                    int valLeftOf = GetSquare(x, y - 1);
+
+                    if (valLeftOf == 0)
+                    {
+                        SetSquare(x, y - 1, GetSquare(x, y));
+                        SetSquare(x, y, 0);
+                        modification = true;
+                    }
+                    else if (valLeftOf == GetSquare(x, y))
+                    {
+                        int value = GetSquare(x, y) * 2;
+                        Score += value;
+                        SetSquare(x, y - 1, value);
+                        SetSquare(x, y, 0);
+                        modification = true;
+                    }
+                }
+            }
+
+            return modification;
+        }
+
+        private bool Right()
+        {
+            bool modification = false;
+
+            for (int y = BOARD_LENGTH - 2; y > 0; y--)
+            {
+                for (int x = BOARD_LENGTH - 1; x > 0; x--)
+                {
+                    int valRightOf = GetSquare(x, y + 1);
+
+                    if (valRightOf == 0)
+                    {
+                        SetSquare(x, y + 1, GetSquare(x, y));
+                        SetSquare(x, y, 0);
+                        modification = true;
+                    }
+                    else if (valRightOf == GetSquare(x, y))
+                    {
+                        int value = GetSquare(x, y) * 2;
+                        Score += value;
+                        SetSquare(x, y + 1, value);
+                        SetSquare(x, y, 0);
+                        modification = true;
+                    }
+                }
+            }
+
+            return modification;
+        }
+
+        private bool Up()
+        {
+            bool modification = false;
+
+            for (int y = 0; y < BOARD_LENGTH; y++)
+            {
+                for (int x = 1; x < BOARD_LENGTH; x++)
+                {
+                    int valAboveOf = GetSquare(x - 1, y);
+
+                    if (valAboveOf == 0)
+                    {
+                        SetSquare(x - 1, y, GetSquare(x, y));
+                        SetSquare(x, y, 0);
+                        modification = true;
+                    }
+                    else if (valAboveOf == GetSquare(x, y))
+                    {
+                        int value = GetSquare(x, y) * 2;
+                        Score += value;
+                        SetSquare(x - 1, y, value);
+                        SetSquare(x, y, 0);
+                        modification = true;
+                    }
+                }
+            }
+
+            return modification;
+        }
+
+        private bool Down()
+        {
+            bool modification = false;
+
+            for (int y = BOARD_LENGTH - 1; y > 0; y--)
+            {
+                for (int x = BOARD_LENGTH - 2; x > 0; x--)
+                {
+                    int valDownOf = GetSquare(x + 1, y);
+
+                    if (valDownOf == 0)
+                    {
+                        SetSquare(x + 1, y, GetSquare(x, y));
+                        SetSquare(x, y, 0);
+                        modification = true;
+                    }
+                    else if (valDownOf == GetSquare(x, y))
+                    {
+                        int value = GetSquare(x, y) * 2;
+                        Score += value;
+                        SetSquare(x + 1, y, value);
+                        SetSquare(x, y, 0);
+                        modification = true;
+                    }
+                }
+            }
+
+            return modification;
+        }
+
+        private void UpdateMaxScore()
+        {
+            for (int x = 0; x < BOARD_LENGTH; x++)
+                for (int y = 0; y < BOARD_LENGTH; y++)
+                    if (GetSquare(x, y) > MaxValue)
+                        MaxValue = GetSquare(x, y);
+        }
+
+        private void CheckForGameOver()
+        {
+            for (int x = 0; x < BOARD_LENGTH; x++)
+                for (int y = 0; y < BOARD_LENGTH; y++)
+                {
+                    var currentSquareValue = GetSquare(x, y);
+                    if (currentSquareValue == 0)
+                        return;
+
+                    if (x > 0 && currentSquareValue == GetSquare(x - 1, y))
+                        return;
+
+                    if (y > 0 && currentSquareValue == GetSquare(x, y - 1))
+                        return;
+                }
+
+            GameOver = true;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < BOARD_LENGTH; i++)
+                sb.Append(" -------");
+
+            sb.Append(" \n\n");
+
+            for (int x = 0; x < BOARD_LENGTH; x++)
+            {
+                for (int y = 0; y < BOARD_LENGTH; y++)
+                {
+                    string val = GetSquare(x, y).ToString();
+                    sb.Append("| " + val);
+                    sb.Append("      ".Substring(val.Length));
+                }                
+
+                sb.Append("|\n\n");
+
+                for (int y = 0; y < BOARD_LENGTH; y++)
+                    sb.Append(" -------");
+                sb.Append(" \n\n");
+            }
+
+            return sb.ToString();
+
+        }
+    }
+}
---
+++ Game2048Solver/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game2048Solver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IGameBoard board = new GameBoard();
+            Random rand = new Random();
+            Array moves = Enum.GetValues(typeof(Direction));
+
+            while (!board.GameOver)
+            {
+                board.Move((Direction)moves.GetValue(rand.Next(moves.Length)));
+            }
+
+            Console.WriteLine("Moves Made:   " + board.Moves);
+            Console.WriteLine("Score:        " + board.Score);
+            Console.WriteLine("Max Value:    " + board.MaxValue);
+            Console.WriteLine();
+            Console.Write(board.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
